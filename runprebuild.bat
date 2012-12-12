a15380185b1beeb5052fa89c5ddb97017968ef91
BlueWall <jamesh@bluewallgroup.com>
2012-12-12 13:12:59 -0500

    Fix path seperators in bat

--- runprebuild.bat
+++ runprebuild.bat
@@ -1,10 +1,10 @@
 C:\WINDOWS\Microsoft.NET\Framework\v3.5\msbuild Mono.Addins.sln /t:clean
 
 rmdir /S /Q bin
-rmdir /S /Q Mono.Addins.CecilReflector/obj
-rmdir /S /Q Mono.Addins.Setup/obj
-rmdir /S /Q Mono.Addins/obj
-rmdir /S /Q mautil/obj
+rmdir /S /Q Mono.Addins.CecilReflector\obj
+rmdir /S /Q Mono.Addins.Setup\obj
+rmdir /S /Q Mono.Addins\obj
+rmdir /S /Q mautil\obj
 
 Prebuild.exe /clean
 Prebuild.exe /target vs2010
